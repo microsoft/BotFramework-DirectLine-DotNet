e66154dfd4460017d3cc5e7e2c41ae1aeebe0f05
CharingJC <74894646+CharingJC@users.noreply.github.com>
2021-03-11 19:35:13 +0800

    Update Program.cs
    Quick Fix

--- samples/core-DirectLineWebSockets/DirectLineClient/Program.cs
+++ samples/core-DirectLineWebSockets/DirectLineClient/Program.cs
@@ -80,7 +80,7 @@
 
             var activitySet = JsonConvert.DeserializeObject<ActivitySet>(e.Data);
             var activities = from x in activitySet.Activities
-                             where x.From.Id == botId
+                             where x.From.Id == "directline-csharp"
                              select x;
 
             foreach (Activity activity in activities)
