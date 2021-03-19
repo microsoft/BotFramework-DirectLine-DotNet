623f0a479ff8535dc6e7bcb9e06ff11aae1152be
CharingJC <74894646+CharingJC@users.noreply.github.com>
2021-03-19 10:48:46 +0800

    Update Program.cs

--- samples/core-DirectLine/DirectLineClient/Program.cs
+++ samples/core-DirectLine/DirectLineClient/Program.cs
@@ -65,7 +65,7 @@
                 watermark = activitySet?.Watermark;
 
                 var activities = from x in activitySet.Activities
-                                 where x.From.Id == "directline-csharp"
+                                 where x.From.Id == botId
                                  select x;
 
                 foreach (Activity activity in activities)
